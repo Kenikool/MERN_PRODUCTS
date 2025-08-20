829982ce662e51a544207e81068681e78d4a2c5d
Kenikool <114859959+Kenikool@users.noreply.github.com>
2025-08-20 18:28:47 +0100

    ERROR 1

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,12 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      process.env.FRONTEND_URL,
+    ],
+
     credentials: true,
   })
 );
