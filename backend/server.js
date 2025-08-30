0bdb7b24a0f7c15909fad109efe273474133c7a8
Kenikool <114859959+Kenikool@users.noreply.github.com>
2025-08-30 14:43:51 +0100

    server and pc

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      process.env.FRONTEND_URL,
-    ],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
 
     credentials: true,
   })
